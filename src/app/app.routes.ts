2e862ff4d1fd24bd047c6b37476a513c9b94c28e
Samy Sammour <samy.sammour@hotmail.com>
2024-05-16 19:29:07 +0300

    Init

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,8 @@
 import { Routes } from '@angular/router';
+import { AppComponent } from './app.component';
+import { WeatherComponent } from './weather/weather.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: '', pathMatch: 'full', component: AppComponent}, 
+    { path: 'weather', component: WeatherComponent}, 
+];
---
+++ src/app/weather/weather.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+
+@Component({
+  selector: 'app-weather',
+  standalone: true,
+  templateUrl: './weather.component.html',
+  styleUrl: './weather.component.scss'
+})
+export class WeatherComponent {
+}
